9797b486822519a21ea945074fafd337de8e5768
Oskar Gewalli <oskar@gewalli.se>
2014-10-22 08:51:24 +0200

    Tests of flatten<T>

--- src/GlobalPhone.Tests/RubyfyTests.cs
+++ src/GlobalPhone.Tests/RubyfyTests.cs
@@ -61,5 +61,24 @@ namespace GlobalPhone.Tests
         {
             Assert.That(actual, Is.EquivalentTo(expected));
         }
+
+        [Test]
+        public void test_flatten_generic()
+        {
+            var a1 = new[] { 1, 2, 3 };
+            var a2 = new[] { 5, 6 };
+            var a3 = new object[] { 4, a2 };
+            var a4 = new object[] { a1, a3 };
+
+            assert_equal(new[] { 1, 2, 3, 4, 5, 6 }, a4.Flatten<int>());
+            assert_equal(new object[] { a1, a3 }, a4);
+
+            var a5 = new object[] { a1, new object[0], a3 };
+            assert_equal(new object[] { 1, 2, 3, 4, 5, 6 }, a5.Flatten<int>());
+            assert_equal(new object[0], new object[0].Flatten<int>());
+            assert_equal(new object[0],
+                           new object[] { new object[] { new object[] { new object[] { }, new object[] { } }, new object[] { new object[] { } }, new object[] { } }, new object[] { new object[] { new object[] { } } } }.Flatten<int>());
+        }
+
     }
 }
